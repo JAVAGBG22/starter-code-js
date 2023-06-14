034628f7f21f38a7a2a3a519290b4cf3bf3d8753
Helena <helena@klarr.se>
2023-06-14 10:42:27 +0200

    instructions for deposit function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // BANK ACCOUNT
+//alert("hejhej");
+/* prompt(
+  "Select a choice 1.) See balance 2.) Make deposit 3.) Withdrawal money 4.) See account name 5.) Exit"
+); */
 
 // REQUIREMENTS
 // Create an object called account that has the following properties:
@@ -32,10 +36,64 @@
 // The atm() function should be responsible for showing the user interface
 // and based on the user input show the right meny choice
 
+// ett objekt som innehåller all vår logik
+const account = {
+  // properties
+  accountName: "Helena Johansson",
+  balance: 100,
+  getAccountName: function () {
+    //alert("Name of account holder: " + this.accountName);
+    // template literal
+    alert(`Name of account holder: ${this.accountName}`);
+
+    atm();
+  },
+  getBalance: function () {},
+  deposit: function () {
+    // se till att spara user input någonstans
+    parseFloat(prompt("How much would you like to deposit?"));
+
+    // efter att user input är sparat bör man kontrollera
+    // så att user input är en giltlig input, alltså ett numer och inget annat.
+    // user input bör inte vara 0 eller negativa tal
+    // && och operatorn
+    // || eller operatorn
+    /* if (test || userTest) {
+        // returneras om villkoret är true
+    } */
+
+    // om det är en korrekt user input då ska balance propertyn öka, alltså
+    // summan som kom från user input ska plussas på balance property
+    // kan vara god console.log(balance)
+  },
+  withdrawal: function () {},
+  exitAccount: function () {},
+};
+
+// let const
+
+// en funktion som endast hanterar att visa rätt val baserat på user input
 function atm() {
   const message = parseFloat(
     prompt(
       "Select a choice 1.) See balance 2.) Make deposit 3.) Withdrawal money 4.) See account name 5.) Exit"
     )
   );
+
+  switch (message) {
+    case 1:
+      console.log("case 1");
+      break;
+    case 2:
+      account.deposit();
+      break;
+    case 3:
+      console.log("case 3");
+      break;
+    case 4:
+      account.getAccountName();
+      break;
+  }
 }
+
+atm();
